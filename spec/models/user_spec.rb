63fd2c36403b788a44fd3729c0952999b482f54d
Keith Koberle <kdkoberle@madisoncollege.edu>
2012-12-19 15:04:00 -0600

    Finished Ch11

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,6 +36,15 @@ describe User do
   it { should be_valid }
   it { should_not be_admin }
   
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
+  
   
   describe "with admin attribute set to 'true'" do
     before do
@@ -183,13 +192,35 @@ describe User do
   it "should destroy associated microposts" do
       microposts = @user.microposts.dup
       @user.destroy
-      microposts.should_not be_empty
+      microposts.should be_empty
       microposts.each do |micropost|
         Micropost.find_by_id(micropost.id).should be_nil
       end
   end
   
-  
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }    
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+    
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+    
+  end
   
   
   
